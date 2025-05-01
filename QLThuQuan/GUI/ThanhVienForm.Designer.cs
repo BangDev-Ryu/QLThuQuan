ebe185a6828b18a600c891e2ddec2dca4b981704
BangDev-Ryu <ryushiki12@gmail.com>
2025-05-01 15:59:58 +0700

    update thanh vien

--- QLThuQuan/GUI/ThanhVienForm.Designer.cs
+++ QLThuQuan/GUI/ThanhVienForm.Designer.cs
@@ -41,6 +41,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnAddLuotVao = new System.Windows.Forms.Button();
+            this.btnQuanLyPhieuMuon = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tableThanhVien)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -106,9 +108,9 @@
             // btnAddThanhVien
             // 
             this.btnAddThanhVien.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddThanhVien.Location = new System.Drawing.Point(64, 35);
+            this.btnAddThanhVien.Location = new System.Drawing.Point(64, 83);
             this.btnAddThanhVien.Name = "btnAddThanhVien";
-            this.btnAddThanhVien.Size = new System.Drawing.Size(129, 40);
+            this.btnAddThanhVien.Size = new System.Drawing.Size(112, 40);
             this.btnAddThanhVien.TabIndex = 1;
             this.btnAddThanhVien.Text = "Thêm thành viên";
             this.btnAddThanhVien.UseVisualStyleBackColor = true;
@@ -116,9 +118,9 @@
             // btnUpdateThanhVien
             // 
             this.btnUpdateThanhVien.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdateThanhVien.Location = new System.Drawing.Point(199, 35);
+            this.btnUpdateThanhVien.Location = new System.Drawing.Point(182, 83);
             this.btnUpdateThanhVien.Name = "btnUpdateThanhVien";
-            this.btnUpdateThanhVien.Size = new System.Drawing.Size(131, 40);
+            this.btnUpdateThanhVien.Size = new System.Drawing.Size(112, 40);
             this.btnUpdateThanhVien.TabIndex = 2;
             this.btnUpdateThanhVien.Text = "Sửa thành viên";
             this.btnUpdateThanhVien.UseVisualStyleBackColor = true;
@@ -126,9 +128,9 @@
             // btnDeleteThanhVien
             // 
             this.btnDeleteThanhVien.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDeleteThanhVien.Location = new System.Drawing.Point(336, 35);
+            this.btnDeleteThanhVien.Location = new System.Drawing.Point(300, 83);
             this.btnDeleteThanhVien.Name = "btnDeleteThanhVien";
-            this.btnDeleteThanhVien.Size = new System.Drawing.Size(131, 40);
+            this.btnDeleteThanhVien.Size = new System.Drawing.Size(112, 40);
             this.btnDeleteThanhVien.TabIndex = 3;
             this.btnDeleteThanhVien.Text = "Xóa thành viên";
             this.btnDeleteThanhVien.UseVisualStyleBackColor = true;
@@ -159,12 +161,34 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "Tìm kiếm";
             // 
+            // btnAddLuotVao
+            // 
+            this.btnAddLuotVao.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddLuotVao.Location = new System.Drawing.Point(856, 33);
+            this.btnAddLuotVao.Name = "btnAddLuotVao";
+            this.btnAddLuotVao.Size = new System.Drawing.Size(112, 40);
+            this.btnAddLuotVao.TabIndex = 6;
+            this.btnAddLuotVao.Text = "Vào khu học tập";
+            this.btnAddLuotVao.UseVisualStyleBackColor = true;
+            // 
+            // btnQuanLyPhieuMuon
+            // 
+            this.btnQuanLyPhieuMuon.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuanLyPhieuMuon.Location = new System.Drawing.Point(738, 33);
+            this.btnQuanLyPhieuMuon.Name = "btnQuanLyPhieuMuon";
+            this.btnQuanLyPhieuMuon.Size = new System.Drawing.Size(112, 40);
+            this.btnQuanLyPhieuMuon.TabIndex = 7;
+            this.btnQuanLyPhieuMuon.Text = "Quản lý phiếu mượn";
+            this.btnQuanLyPhieuMuon.UseVisualStyleBackColor = true;
+            // 
             // ThanhVienForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.btnQuanLyPhieuMuon);
+            this.Controls.Add(this.btnAddLuotVao);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnDeleteThanhVien);
             this.Controls.Add(this.btnUpdateThanhVien);
@@ -197,5 +221,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddLuotVao;
+        private System.Windows.Forms.Button btnQuanLyPhieuMuon;
     }
 }
