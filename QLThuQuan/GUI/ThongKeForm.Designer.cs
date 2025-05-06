9804d4cce3a70a8731ea37702209a7f3fea17288
BangDev-Ryu <ryushiki12@gmail.com>
2025-05-06 13:27:12 +0700

    update thong ke

--- QLThuQuan/GUI/ThongKeForm.Designer.cs
+++ QLThuQuan/GUI/ThongKeForm.Designer.cs
@@ -28,34 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 10D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 6D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(3D, 13D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, 15D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint5 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, 9D);
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pnChart = new System.Windows.Forms.Panel();
+            this.pkStartDate = new System.Windows.Forms.DateTimePicker();
+            this.pkEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.pnChart.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // chart1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(466, 306);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Thống kê";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Lượt vào";
+            series1.Points.Add(dataPoint1);
+            series1.Points.Add(dataPoint2);
+            series1.Points.Add(dataPoint3);
+            series1.Points.Add(dataPoint4);
+            series1.Points.Add(dataPoint5);
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(450, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "LuotVao";
+            title1.Text = "Lượt vào theo thời gian";
+            this.chart1.Titles.Add(title1);
+            // 
+            // pnChart
+            // 
+            this.pnChart.Controls.Add(this.chart1);
+            this.pnChart.Location = new System.Drawing.Point(35, 307);
+            this.pnChart.Name = "pnChart";
+            this.pnChart.Size = new System.Drawing.Size(450, 300);
+            this.pnChart.TabIndex = 1;
+            // 
+            // pkStartDate
+            // 
+            this.pkStartDate.Location = new System.Drawing.Point(35, 254);
+            this.pkStartDate.Name = "pkStartDate";
+            this.pkStartDate.Size = new System.Drawing.Size(77, 20);
+            this.pkStartDate.TabIndex = 2;
+            // 
+            // pkEndDate
+            // 
+            this.pkEndDate.Location = new System.Drawing.Point(35, 280);
+            this.pkEndDate.Name = "pkEndDate";
+            this.pkEndDate.Size = new System.Drawing.Size(77, 20);
+            this.pkEndDate.TabIndex = 3;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(129, 273);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(83, 27);
+            this.btnThongKe.TabIndex = 4;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(35, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 150);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(285, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 150);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(535, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 150);
+            this.panel3.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(785, 50);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 150);
+            this.panel4.TabIndex = 8;
             // 
             // ThongKeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1048, 642);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.pkEndDate);
+            this.Controls.Add(this.pkStartDate);
+            this.Controls.Add(this.pnChart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ThongKeForm";
             this.Text = "ThongKeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.pnChart.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel pnChart;
+        private System.Windows.Forms.DateTimePicker pkStartDate;
+        private System.Windows.Forms.DateTimePicker pkEndDate;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
